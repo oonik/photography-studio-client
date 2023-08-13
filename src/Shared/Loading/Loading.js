9cb112ea71ddeca08fdbd0ff537eee567c105db9
oonik <aniksarkar0177@gmail.com>
2023-08-13 14:29:56 +0600

    update loading component

--- src/Shared/Loading/Loading.js
+++ src/Shared/Loading/Loading.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Loading = () => {
     return (
         <div>
-            <h1 className='text-cyan-400 text-center mt-20'>Loading<span className="loading loading-dots loading-lg"></span></h1>
+            <div className='text-blue-500 text-center my-20'>
+            <p className='text-2xl'> Loading
+            <span className="loading loading-ball loading-xs"></span>
+            <span className="loading loading-ball loading-sm"></span>
+            <span className="loading loading-ball loading-md"></span>
+            <span className="loading loading-ball loading-lg"></span>
+            </p>
+        </div>
         </div>
     );
 };
